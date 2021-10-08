194dc069c842908f580e91d2351c4c8bac55fb8a
Slava Pavlutin <sl.pavlutin@gmail.com>
2021-10-08 19:28:59 +0300

    init commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
 import type { NextPage } from 'next';
+import tw, { theme } from 'twin.macro';
 
 let Home: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <div css={[tw`text-rose-300`]}>
+      <svg viewBox="0 0 60 17" fill="currentColor" width="300">
+        <defs>
+          <linearGradient id="grad">
+            <stop offset="0" stopColor={theme`colors.rose.300`} />
+            <stop offset="1" stopColor={theme`colors.rose.400`} />
+          </linearGradient>
+        </defs>
+        <text y="15" fontFamily="Pacifico" fill="url(#grad)">
+          lovebirds
+        </text>
+      </svg>
+    </div>
+  );
 };
 
 export default Home;
