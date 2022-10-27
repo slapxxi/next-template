c59a6bd818a670c6f65b71cf569f390de9a74dc3
Slava Pavlutin <sl.pavlutin@gmail.com>
2022-10-27 19:55:02 +0300

    update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: 'jit',
+  purge: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: { ...colors },
