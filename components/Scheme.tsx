e390bd90a9c42fb09c569401202961e49bfdc987
Slava Pavlutin <sl.pavlutin@gmail.com>
2023-02-06 21:39:16 +0300

    reuse styles in Scheme

--- components/Scheme.tsx
+++ components/Scheme.tsx
@@ -4,6 +4,7 @@ import { CodeIcon } from './icons/CodeIcon';
 import { CogIcon } from './icons/CogIcon';
 import { MessageIcon } from './icons/MessageIcon';
 import { List, ListItem } from './List';
+import styles from './Scheme.module.scss';
 import { Title } from './Title';
 
 type Ids = 'functions' | 'tuning' | 'support' | 'expertise';
@@ -26,7 +27,7 @@ export let Scheme = () => {
       >
         <a
           id="functions"
-          className="group data-checked:text-white"
+          className={`${styles.link}`}
           onClick={handleClick}
           data-checked={selectedId === 'functions'}
         >
@@ -36,38 +37,36 @@ export let Scheme = () => {
             fill="#CAD2FF"
             rx="8"
             transform="translate(115 200) rotate(30 45 45) "
-            className="group-data-checked:hidden"
+            className={`${styles.backdrop}`}
+            style={{
+              transform:
+                selectedId === 'functions'
+                  ? 'translate(115px, 200px) rotate(45deg)'
+                  : 'translate(115px, 200px) rotate(30deg)',
+              transformOrigin: '45px 45px',
+            }}
           />
           <rect
             width="98"
             height="98"
             fill="#fff"
             stroke="#0862EE"
-            rx="7.47"
-            className="stroke-2 group-data-checked:hidden"
+            rx="8"
+            className={`${styles.tile}`}
             transform="translate(115 200) rotate(45 45 45)"
           />
           <rect
             filter={`url(#${filterId})`}
             width="98"
             height="98"
-            rx="7.47"
-            className="hidden fill-navy stroke-2 group-data-checked:block"
+            rx="8"
+            className={`${styles.activeTile}`}
             transform="translate(115 200) rotate(45 45 45)"
           />
-          <g
-            className="transition-transform duration-75 group-data-checked:translate-y-3 
-						group-data-checked:scale-125 group-data-checked:duration-300"
-            style={{ transformOrigin: '166px 210px' }}
-          >
+          <g className={`${styles.icon}`} style={{ transformOrigin: '166px 210px' }}>
             <CogIcon size={42} x={140} y={210} />
           </g>
-          <text
-            fill="currentColor"
-            x="118"
-            y="272"
-            className="transition-opacity group-data-checked:opacity-0"
-          >
+          <text fill="currentColor" x="118" y="272" className={`${styles.text}`}>
             Функционал
           </text>
         </a>
@@ -75,8 +74,8 @@ export let Scheme = () => {
         <a
           id="expertise"
           onClick={handleClick}
-          className="group data-checked:text-white"
           data-checked={selectedId === 'expertise'}
+          className={`${styles.link}`}
         >
           <rect
             width="99"
@@ -84,38 +83,36 @@ export let Scheme = () => {
             fill="#CAD2FF"
             rx="8"
             transform="translate(115 20) rotate(30 45 45) "
-            className="group-data-checked:hidden"
+            className={`${styles.backdrop}`}
+            style={{
+              transform:
+                selectedId === 'expertise'
+                  ? 'translate(115px, 20px) rotate(45deg)'
+                  : 'translate(115px, 20px) rotate(30deg)',
+              transformOrigin: '45px 45px',
+            }}
           />
           <rect
             width="98"
             height="98"
             fill="#fff"
             stroke="#0862EE"
-            rx="7.47"
-            className="stroke-2 group-data-checked:hidden"
+            rx="8"
+            className={`${styles.tile}`}
             transform="rotate(45 80 195)"
           />
           <rect
             filter={`url(#${filterId})`}
             width="98"
             height="98"
-            rx="7.47"
-            className="hidden fill-navy stroke-2 group-data-checked:block"
+            rx="8"
+            className={`${styles.activeTile}`}
             transform="translate(116 19) rotate(45 45 45)"
           />
-          <g
-            className="transition-transform duration-75 group-data-checked:translate-y-3 
-						group-data-checked:scale-125 group-data-checked:duration-300"
-            style={{ transformOrigin: '153px 30px' }}
-          >
+          <g style={{ transformOrigin: '156px 30px' }} className={`${styles.icon}`}>
             <ChartIcon size={38} x={143} y={30} />
           </g>
-          <text
-            fill="currentColor"
-            x="120"
-            y="86"
-            className="transition-opacity group-data-checked:opacity-0"
-          >
+          <text fill="currentColor" x="120" y="86" className={`${styles.text}`}>
             Экспертиза
           </text>
         </a>
@@ -123,103 +120,101 @@ export let Scheme = () => {
         <a
           id="support"
           onClick={handleClick}
-          className="group data-checked:text-white"
           data-checked={selectedId === 'support'}
+          className={`${styles.link}`}
         >
           <rect
             width="99"
             height="99"
             fill="#CAD2FF"
             rx="8"
-            transform="rotate(30 -57 478)"
-            className="group-data-checked:hidden"
+            className={`${styles.backdrop}`}
+            style={{
+              transform:
+                selectedId === 'support'
+                  ? 'translate(205px, 110px) rotate(45deg)'
+                  : 'translate(205px, 110px) rotate(30deg)',
+              transformOrigin: '45px 45px',
+            }}
           />
           <rect
-            width="97.92"
-            height="97.92"
-            y="1.17"
+            width="99"
+            height="99"
             fill="#fff"
             stroke="#0862EE"
-            stroke-width="1.66"
-            rx="7.47"
+            rx="8"
             transform="rotate(45.02 15.62 347.6)"
-            className="group-data-checked:hidden"
+            className={`${styles.tile}`}
           />
           <rect
             filter={`url(#${filterId})`}
             width="98"
             height="98"
-            rx="7.47"
-            className="hidden fill-navy stroke-2 group-data-checked:block"
+            rx="8"
             transform="translate(205 110) rotate(45 45 45)"
+            className={`${styles.activeTile}`}
           />
-          <g
-            className="transition-transform duration-75 group-data-checked:translate-y-3 
-						group-data-checked:scale-125 group-data-checked:duration-300"
-            style={{ transformOrigin: '241px 120px' }}
-          >
+          <g className={`${styles.icon}`} style={{ transformOrigin: '244px 120px' }}>
             <MessageIcon size={38} x={231} y={120} />
           </g>
-          <text
-            fill="currentColor"
-            x="209"
-            y="176"
-            className="transition-opacity group-data-checked:opacity-0"
-          >
+          <text fill="currentColor" x="209" y="176" className={`${styles.text}`}>
             Поддержка
           </text>
         </a>
 
-        <a id="tuning" onClick={handleClick} className="group" data-checked={selectedId === 'tuning'}>
+        <a
+          id="tuning"
+          onClick={handleClick}
+          data-checked={selectedId === 'tuning'}
+          className={`${styles.link}`}
+        >
           <rect
-            width="99.57"
-            height="99.6"
+            width="99"
+            height="99"
             fill="#CAD2FF"
-            rx="8.3"
-            transform="rotate(30.01 -147.15 143.76) skewX(.03)"
+            rx="8"
+            className="transition-transform"
+            style={{
+              transform:
+                selectedId === 'tuning'
+                  ? 'translate(25px,110px) rotate(45deg)'
+                  : 'translate(25px,110px) rotate(30deg)',
+              transformOrigin: '45px 45px',
+            }}
           />
           <rect
-            width="97.92"
-            height="97.92"
-            y="1.17"
+            width="98"
+            height="98"
             fill="#fff"
             stroke="#0862EE"
-            stroke-width="1.66"
-            rx="7.47"
+            stroke-width="2"
+            rx="8"
             transform="rotate(45.02 -73.98 131.36)"
+            className={`${styles.tile}`}
           />
-          <CodeIcon size={38} x={50} y={120} />
-          <text fill="currentColor" x="33" y="176">
+          <rect
+            filter={`url(#${filterId})`}
+            width="98"
+            height="98"
+            rx="8"
+            className={`${styles.activeTile}`}
+            transform="translate(25 110) rotate(45 45 45)"
+          />
+
+          <g className={`${styles.icon}`} style={{ transformOrigin: '70px 120px' }}>
+            <CodeIcon size={38} x={50} y={120} />
+          </g>
+          <text fill="currentColor" x="33" y="176" className={`${styles.text}`}>
             Доработки
           </text>
         </a>
 
         <g>
-          <path
-            stroke="#CAD2FF"
-            stroke-linecap="round"
-            d="M.5-.5h293.58"
-            transform="rotate(45.02 -41.25 96.46)"
-          />
-          <path
-            stroke="#CAD2FF"
-            stroke-linecap="round"
-            d="M.5-.5h293.58"
-            transform="rotate(-45.02 348.6 65.07)"
-          />
+          <path stroke="#CAD2FF" d="M.5-.5h293.58" transform="rotate(45 -41 96)" className="cap-round" />
+          <path stroke="#CAD2FF" d="M.5-.5h293.58" transform="rotate(-45 348 65)" className="cap-round" />
           <path fill="#0862EE" d="M144.36 145.27h31.53v31.54h-31.53z" />
-          <path
-            stroke="#fff"
-            stroke-linecap="square"
-            stroke-width="4.98"
-            d="M151.6 151.9v.61l17.04 17.05v.6"
-          />
-          <path
-            stroke="#fff"
-            stroke-linecap="square"
-            stroke-width="4.98"
-            d="M168.64 151.9v.61l-17.04 17.05v.6"
-          />
+          <path stroke="#fff" strokeLinecap="square" strokeWidth="5" d="M151.6 151.9v.61l17.04 17.05v.6" />
+          <path stroke="#fff" strokeLinecap="square" strokeWidth="5" d="M168.64 151.9v.61l-17.04 17.05v.6" />
         </g>
 
         <defs>
@@ -261,8 +256,8 @@ export let Scheme = () => {
               Доработки
             </Title>
             <List className="ml-5  text-xs font-normal" variant="disc">
-              <ListItem>18 модулей</ListItem>
               <ListItem>Полный цикл управления портфелем</ListItem>
+              <ListItem>18 модулей</ListItem>
               <ListItem>Безопасность</ListItem>
               <ListItem>CRM</ListItem>
             </List>
@@ -274,9 +269,9 @@ export let Scheme = () => {
               Экспертиза
             </Title>
             <List className="ml-5  text-xs font-normal" variant="disc">
-              <ListItem>18 модулей</ListItem>
               <ListItem>Полный цикл управления портфелем</ListItem>
               <ListItem>Безопасность</ListItem>
+              <ListItem>18 модулей</ListItem>
               <ListItem>CRM</ListItem>
             </List>
           </>
@@ -287,10 +282,10 @@ export let Scheme = () => {
               Поддержка
             </Title>
             <List className="ml-5  text-xs font-normal" variant="disc">
-              <ListItem>18 модулей</ListItem>
               <ListItem>Полный цикл управления портфелем</ListItem>
               <ListItem>Безопасность</ListItem>
               <ListItem>CRM</ListItem>
+              <ListItem>18 модулей</ListItem>
             </List>
           </>
         )}
