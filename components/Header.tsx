1bfcc85436a1a20247b6db9a79fbc2b291d75323
Slava Pavlutin <sl.pavlutin@gmail.com>
2023-03-02 18:06:13 +0300

    use react-remove-scroll

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,11 @@
 import classNames from 'classnames';
+import { RemoveScroll } from 'react-remove-scroll';
 import * as Select from '@radix-ui/react-select';
 import { ChevronDown, ChevronUp, Check, Phone, Mail } from 'lucide-react';
 import { useState } from 'react';
 import Link from 'next/link';
 import { Logo } from 'components/Logo';
 import { Modal } from 'components/Modal';
-import { useNoScroll } from 'lib/hooks/useNoScroll';
 
 export type HeaderProps = {
   children?: React.ReactNode;
@@ -16,7 +16,7 @@ export const Header = (props: HeaderProps) => {
   const { children, className = '', ...rest } = props;
   const [menuOpen, setMenuOpen] = useState(false);
 
-  useNoScroll(menuOpen);
+  // useNoScroll(menuOpen);
 
   function handleClickMenu() {
     setMenuOpen((o) => !o);
@@ -84,96 +84,97 @@ export const Header = (props: HeaderProps) => {
       </button>
 
       <Modal open>
-        <nav
-          className={classNames(
-            'fixed inset-0 z-10 flex flex-col',
-            menuOpen ? 'translate-x-0' : 'translate-x-full',
-          )}
-        >
-          <header className="header">
-            <Logo className="header__logo" />
-
-            {/* <button className="ml-auto text-white" onClick={handleClickMenu}>
-              <svg width={26} fill="none" viewBox="0 0 26 23">
-                <path d="M0 0H26V1H0Z" className="menu__buttonTop" />
-                <path d="M3 11H26V12H3Z" className="menu__buttonCenter" />
-                <path d="M0 22H26V23H0Z" className="menu__buttonBottom" />
-              </svg>
-            </button> */}
-
-            <button className="ml-auto text-white" onClick={handleClickMenu}>
-              <svg width={26} fill="none" viewBox="0 0 26 23">
-                <path
-                  d="M0 0H26V1H0Z"
-                  className={classNames('fill-current transition-transform')}
-                  style={{ transform: menuOpen ? 'translate(4px,1px) rotate(45deg)' : 'none' }}
-                />
-                <path
-                  d="M3 11H26V12H3Z"
-                  className={classNames('fill-current transition-transform', menuOpen && 'translate-x-full')}
-                />
-                <path
-                  d="M0 22H26V23H0Z"
-                  className={classNames('fill-current transition-transform')}
-                  style={{ transform: menuOpen ? 'translate(-13px,4px) rotate(-45deg)' : 'none' }}
-                />
-              </svg>
-            </button>
-          </header>
-
-          <div
+        <RemoveScroll enabled={menuOpen}>
+          <nav
             className={classNames(
-              'flex-1 border-b border-navy-800 bg-navy-900 py-12 transition-all duration-300',
-              menuOpen ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0',
+              'fixed inset-0 z-10 flex flex-col',
+              menuOpen ? 'translate-x-0' : 'translate-x-full',
             )}
           >
-            <ul className="flex flex-col items-center gap-7">
-              <li>
-                <Link href="/" className="navText">
-                  Главная
-                </Link>
-              </li>
-              <li>
-                <Link href="/" className="navText">
-                  Наши склады
-                </Link>
-              </li>
-              <li>
-                <Link href="/" className="navText">
-                  Брокерам
-                </Link>
-              </li>
-              <li>
-                <Link href="/" className="navText">
-                  Контакты
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <footer
-            className={classNames(
-              'bg-navy-900 py-7 px-4 transition-all duration-300',
-              menuOpen ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0',
-            )}
-          >
-            <ul className="flex flex-col gap-4">
-              <li className="flex items-center gap-3.5 text-sm text-navy-400">
-                <Phone size={12} className="fill-current" />
-                <span>+7 (495) 737 8585</span>
-              </li>
-              <li>
-                <a
-                  href="mailto:ffkaragandainfo@info.ru"
-                  className="flex items-center gap-3.5 text-sm text-navy-400"
-                >
-                  <Mail size={12} />
-                  <span>ffkaragandainfo@info.ru</span>
-                </a>
-              </li>
-            </ul>
-          </footer>
-        </nav>
+            <header className="header">
+              <Logo className="header__logo" />
+              {/* <button className="ml-auto text-white" onClick={handleClickMenu}>
+                <svg width={26} fill="none" viewBox="0 0 26 23">
+                  <path d="M0 0H26V1H0Z" className="menu__buttonTop" />
+                  <path d="M3 11H26V12H3Z" className="menu__buttonCenter" />
+                  <path d="M0 22H26V23H0Z" className="menu__buttonBottom" />
+                </svg>
+              </button> */}
+              <button className="ml-auto text-white" onClick={handleClickMenu}>
+                <svg width={26} fill="none" viewBox="0 0 26 23">
+                  <path
+                    d="M0 0H26V1H0Z"
+                    className={classNames('fill-current transition-transform')}
+                    style={{ transform: menuOpen ? 'translate(4px,1px) rotate(45deg)' : 'none' }}
+                  />
+                  <path
+                    d="M3 11H26V12H3Z"
+                    className={classNames(
+                      'fill-current transition-transform',
+                      menuOpen && 'translate-x-full',
+                    )}
+                  />
+                  <path
+                    d="M0 22H26V23H0Z"
+                    className={classNames('fill-current transition-transform')}
+                    style={{ transform: menuOpen ? 'translate(-13px,4px) rotate(-45deg)' : 'none' }}
+                  />
+                </svg>
+              </button>
+            </header>
+            <div
+              className={classNames(
+                'flex-1 border-b border-navy-800 bg-navy-900 py-12 transition-all duration-300',
+                menuOpen ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0',
+              )}
+            >
+              <ul className="flex flex-col items-center gap-7">
+                <li>
+                  <Link href="/" className="navText">
+                    Главная
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" className="navText">
+                    Наши склады
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" className="navText">
+                    Брокерам
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/" className="navText">
+                    Контакты
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <footer
+              className={classNames(
+                'bg-navy-900 py-7 px-4 transition-all duration-300',
+                menuOpen ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0',
+              )}
+            >
+              <ul className="flex flex-col gap-4">
+                <li className="flex items-center gap-3.5 text-sm text-navy-400">
+                  <Phone size={12} className="fill-current" />
+                  <span>+7 (495) 737 8585</span>
+                </li>
+                <li>
+                  <a
+                    href="mailto:ffkaragandainfo@info.ru"
+                    className="flex items-center gap-3.5 text-sm text-navy-400"
+                  >
+                    <Mail size={12} />
+                    <span>ffkaragandainfo@info.ru</span>
+                  </a>
+                </li>
+              </ul>
+            </footer>
+          </nav>
+        </RemoveScroll>
       </Modal>
     </header>
   );
